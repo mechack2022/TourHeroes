9476dd561d1568c330ec0d1b4510f8d546a239a6
mechack200 <taiwogboyegun@gmmail.com>
2022-04-07 05:22:57 +0200

    heroes services implemented

--- src/app/heroes.service.ts
+++ src/app/heroes.service.ts
@@ -1,9 +1,26 @@
+import { HEROES } from './mock.hero';
+import { MessagesService } from './messages.service';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HeroModel } from './heroes.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
+// hero : HeroModel[] = []
+  constructor(private messageService:MessagesService) { }
 
-  constructor() { }
+
+  getHeroes():Observable<HeroModel[]>{
+   this.messageService.addMessages(`hero-services: heroes fetched`)
+   return of(HEROES)
+  }
+ 
+  getHero(id:number): Observable<HeroModel>{  
+    const hero = HEROES.find(hero => hero.id === id)!;            
+     this.messageService.addMessages(`HeroService fetcted hero ${id}`);
+      return of(hero)
+
+}
 }
--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MessagesService {
-
+ messages :string[]=[]
   constructor() { }
+
+  addMessages(message:string){
+    this.messages.push(message)
+  }
+  clearMessges(){
+    this.messages = []
+  }
 }
