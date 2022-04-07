18ecdab0f48e31053ec9dee92782ebbc08981a66
mechack200 <taiwogboyegun@gmmail.com>
2022-04-07 06:59:44 +0200

    hero-details implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -18,6 +18,7 @@ import { HeroDetailsComponent } from './hero-details/hero-details.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { HEROES } from './../mock.hero';
+import { HeroModel } from './../heroes.model';
+import { HeroesService } from './../heroes.service';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-details',
@@ -6,10 +11,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-details.component.css']
 })
 export class HeroDetailsComponent implements OnInit {
+hero  : HeroModel | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute, 
+    private heroesService:HeroesService,
+    private location:Location
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.getHero()
+  }   
+  getHero():void{
+    const id = Number( this.route.snapshot.paramMap.get('id'))
+    this.heroesService.getHero(id)
+    .subscribe(hero  => this.hero = hero)
+    }
+    getBack():void{
+      this.location.back()
+    }
 
 }
